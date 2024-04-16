824110536408d2dca83f599f0e93239f639dc520
Tasank <apanasovich04@mail.ru>
2024-04-16 17:18:31 +0500

    создание приложения

--- news/news/settings.py
+++ news/news/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django.contrib.flatpages'
+    'django.contrib.flatpages',
     'news',
     'accounts',
     'fpages'
---
+++ news/simp_dungeon/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SimpDungeonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'simp_dungeon'
