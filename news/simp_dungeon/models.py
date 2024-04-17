82125519362da91bed9222c584b6c919a39a68ce
Tasank <apanasovich04@mail.ru>
2024-04-17 20:55:59 +0500

    правка миграций

--- news/simp_dungeon/models.py
+++ news/simp_dungeon/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Order(models.Model):
-    time_in = models.DateField(auto_now_add =True)
-    time_out = models.DateField(auto_now = True)
-    cost = models.FloatField()
-
-class Products_Orders(models.Model):
-    pass
-
-class Staff(models.Model):
-    full_name = models.CharField(max_length = 150)
-    position = models.CharField(max_length = 150)
-    labor_contract = models.IntegerField()
-
-
-class Product(models.Model):
-    name = models.CharField(max_length = 255)
-    price = models.FloatField(default = 0.0)
