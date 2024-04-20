cdb95b254f359fe9a363d1c76ddb0c1d6a127050
Tasank <apanasovich04@mail.ru>
2024-04-20 21:37:47 +0500

    добавил модули к News

--- news/simp_dungeon/models.py
+++ news/simp_dungeon/models.py
@@ -3,5 +3,16 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-class News(models.Model):
+class Author(models.Model):
+    pass
+
+class Category(models.Model):
+    Title = models.CharField(max_length=100, unique=True)
+
+class Post(models.Model):
+    pass
+
+class PostCategory(models.Model):
+    pass
+class Comment(models.Model):
     pass
