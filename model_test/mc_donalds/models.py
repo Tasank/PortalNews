48ec607ae9936a13bc04911ebbbb70fff51f08c0
Tasank <apanasovich04@mail.ru>
2024-04-25 20:46:57 +0500

    git
    Signed-off-by: Tasank <apanasovich04@mail.ru>

---
+++ model_test/mc_donalds/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+director = 'DI'
+admin = 'AD'
+cook = 'CO'
+cashier = 'CA'
+cleaner = 'CL'
+
+POSITIONS = [
+    (director, 'Директор'),
+    (admin, 'Администратор'),
+    (cook, 'Повар'),
+    (cashier, 'Кассир'),
+    (cleaner, 'Уборщик')
+]
+
+
+class Staff(models.Model):
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=2,
+                                choices=POSITIONS,
+                                default=cashier)
+    labor_contract = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+    composition = models.TextField(default="Состав не указан")
+
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+
+    products = models.ManyToManyField(Product, through='ProductOrder')
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
