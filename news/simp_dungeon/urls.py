22167fb7f3a5662c88d340f94241f7b32cfbf3c8
Tasank <apanasovich04@mail.ru>
2024-04-26 20:31:40 +0500

    Добавление в News
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- news/simp_dungeon/urls.py
+++ news/simp_dungeon/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import ProductsList
+from .views import PostList, PostDetail
 
 
 urlpatterns = [
@@ -10,5 +10,8 @@ urlpatterns = [
    # Т.к. наше объявленное представление является классом,
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
-   path('', ProductsList.as_view()),
+   path('', PostList.as_view()),
+    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
+   # int — указывает на то, что принимаются только целочисленные значения
+   path('<int:pk>', PostDetail.as_view()),
 ]
