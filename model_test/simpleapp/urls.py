6fa3f427db8a56ee2758a6e678706dd0756b0e13
Tasank <apanasovich04@mail.ru>
2024-04-26 20:01:00 +0500

    Добавление
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- model_test/simpleapp/urls.py
+++ model_test/simpleapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import ProductsList
+from .views import ProductsList, ProductDetail
 
 
 urlpatterns = [
@@ -11,4 +11,7 @@ urlpatterns = [
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
    path('', ProductsList.as_view()),
+    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
+   # int — указывает на то, что принимаются только целочисленные значения
+   path('<int:pk>', ProductDetail.as_view()),
 ]
--- model_test/simpleapp/views.py
+++ model_test/simpleapp/views.py
@@ -1,6 +1,6 @@
 # Импортируем класс, который говорит нам о том,
 # что в этом представлении мы будем выводить список объектов из БД
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 
@@ -11,7 +11,18 @@ class ProductsList(ListView):
     ordering = 'name'
     # Указываем имя шаблона, в котором будут все инструкции о том,
     # как именно пользователю должны быть показаны наши объекты
-    template_name = 'flatpages/products.html'
+    template_name = 'products.html'
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'products'
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Product
+    # Используем другой шаблон — product.html
+    template_name = 'product.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'product'
