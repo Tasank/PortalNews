b7cf197d5271e5ffc572a98b56f70059f14ecd25
Tasank <apanasovich04@mail.ru>
2024-04-28 19:52:22 +0500

    Изменения в news
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- news/simp_dungeon/views.py
+++ news/simp_dungeon/views.py
@@ -2,19 +2,21 @@
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 
 class PostList(ListView):
     # Указываем модель, объекты которой мы будем выводить
     model = Post
     # Поле, которое будет использоваться для сортировки объектов
-    ordering = 'title'
+    ordering = ['-post_time']
     # Указываем имя шаблона, в котором будут все инструкции о том,
     # как именно пользователю должны быть показаны наши объекты
     template_name = 'news.html'
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
+
 
 class PostDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
@@ -23,3 +25,4 @@ class PostDetail(DetailView):
     template_name = 'the_news.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'the_news'
+
