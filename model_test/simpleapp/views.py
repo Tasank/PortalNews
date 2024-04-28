498f1f441d58022421a9f94c4cdcaec1cbe6881e
Tasank <apanasovich04@mail.ru>
2024-04-28 18:38:50 +0500

    MODEL_TEST
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- model_test/simpleapp/views.py
+++ model_test/simpleapp/views.py
@@ -2,6 +2,7 @@
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Product
+from datetime import datetime
 
 
 class ProductsList(ListView):
@@ -15,6 +16,21 @@ class ProductsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = None
+        return context
 
 class ProductDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
