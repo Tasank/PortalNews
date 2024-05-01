ac6b8d172d7da474c6d631847ade559a3921d8fb
Tasank <apanasovich04@mail.ru>
2024-05-01 22:13:46 +0500

    тотальная цензура слов
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- news/simp_dungeon/templatetags/custom_filters.py
+++ news/simp_dungeon/templatetags/custom_filters.py
@@ -1,23 +1,36 @@
 from django import template
 from pymystem3 import Mystem
+from pygtrie import Trie
+import os
 # Импортируем стороннюю библиотеку которая будет цензурить русский язык
 # pymystem3 - мощный инструмент для работы с русским языком
+# Trie - будет уменьшать сложность
+# os - нужен для открытия файла с нежелательными словами
+
 register = template.Library()
 mystem = Mystem()
 
 @register.filter
 def censor(value):
     if isinstance(value, str):
-        lemmas = mystem.lemmatize(value)
-        censored_value = []
-        ban_words = ['редиска']
-        for lemma in lemmas:
-            for ban_word in ban_words:
-                if ban_word in lemma.lower():
-                    censored_value.append("*" * len(lemma))
-                    break
-                else:
-                    censored_value.append(lemma)
-        return "".join(censored_value)
-    else:
-        return value
+        # Загружаем список матерных слов в Trie
+        censored_words = Trie()
+
+        file_path = os.path.join(os.path.dirname(__file__), 'censored_words.txt')
+        # 'r' означает на режим открытия файла
+        # Чтобы всё заработало нужно указать кодировку encoding='utf-8'
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for word in file:
+                censored_words[word.strip()] = True
+        words = mystem.lemmatize(value)
+        censored_text = []
+        # добавление в новый список цензурованного текста
+        for word in words:
+            if word.strip() in censored_words:
+                censored_text.append('*' * len(word))
+            else:
+                censored_text.append(word)
+
+        return ''.join(censored_text)
+
+    return value
