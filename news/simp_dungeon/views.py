bb4ebaabe37e154dc79e99b08d6b572df66e1690
Tasank <apanasovich04@mail.ru>
2024-05-03 20:56:45 +0500

    Изменения в news
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- news/simp_dungeon/views.py
+++ news/simp_dungeon/views.py
@@ -3,6 +3,7 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
 from datetime import datetime
+from .filters import PostFilter
 
 
 class PostList(ListView):
@@ -17,6 +18,17 @@ class PostList(ListView):
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
     paginate_by = 2
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['filterset'] = self.filterset
+        context['next_sale'] = None
+        return context
 
 
 class PostDetail(DetailView):
@@ -26,4 +38,5 @@ class PostDetail(DetailView):
     template_name = 'the_news.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'the_news'
+
 
