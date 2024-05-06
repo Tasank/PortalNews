14c62b0bcf58b2d6b5d654f9ee864b7d6c2343dc
Tasank <apanasovich04@mail.ru>
2024-05-06 21:22:37 +0500

    формы
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- model_test/simpleapp/forms.py
+++ model_test/simpleapp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Product
 
 class ProductForm(forms.ModelForm):
@@ -11,3 +13,20 @@ class ProductForm(forms.ModelForm):
            'category',
            'price',
        ]
+
+   def clean(self):
+
+       cleaned_data = super().clean()
+       description = cleaned_data.get("description")
+       if description is not None and len(description) < 20:
+           raise ValidationError({
+               "description": "Описание не может быть менее 20 символов."
+           })
+
+
+       name = cleaned_data.get("name")
+       if name == description:
+           raise ValidationError(
+               "Описание не должно быть идентичным названию."
+           )
+       return cleaned_data
--- news/simp_dungeon/forms.py
+++ news/simp_dungeon/forms.py
@@ -1,7 +1,30 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Post
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'type', 'category', 'text']
+        fields = [
+            'title',
+            'type',
+            'category',
+            'text'
+        ]
+
+    def clean(self):
+
+        cleaned_data = super().clean()
+        text = cleaned_data.get("text")
+        if text is not None and len(text) < 40:
+            raise ValidationError({
+                "text": "Описание не может быть менее 40 символов."
+            })
+
+        title = cleaned_data.get("title")
+        if title == text:
+            raise ValidationError(
+                "Описание не должно быть идентичным названию."
+            )
+        return cleaned_data
