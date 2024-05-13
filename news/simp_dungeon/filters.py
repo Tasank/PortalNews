c82347a8b32e52eebf86306cd72ad1dc48dfdec4
Tasank <apanasovich04@mail.ru>
2024-05-13 19:05:08 +0500

    Работа с news
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- news/simp_dungeon/filters.py
+++ news/simp_dungeon/filters.py
@@ -1,11 +1,11 @@
 from django_filters import FilterSet, DateFilter, CharFilter
-from django_filters.widgets import RangeWidget
 from .models import Post
-
+from django import forms
 class PostFilter(FilterSet):
     title = CharFilter(field_name='title', lookup_expr='icontains', label='Название')
     author = CharFilter(field_name='author__user__username', lookup_expr='icontains', label='Имя автора')
-    post_time = DateFilter(field_name='post_time', lookup_expr='gt', widget=RangeWidget(attrs={'type': 'date'}), label='Поиск по дате')
+    post_time = DateFilter(field_name='post_time', lookup_expr='gt', widget=forms.DateInput(attrs={'type': 'date'}),
+                           label='Поиск по дате')
 
     class Meta:
         model = Post
