967478ae69cd4c33ebdc76372a2b16a7b5421ff9
Tasank <apanasovich04@mail.ru>
2024-06-11 14:50:55 +0500

    Кэш
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- news/news/settings.py
+++ news/news/settings.py
@@ -208,3 +208,10 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'), # Указываем, куда будем сохранять кэшируемые файлы
+    }
+}
--- news/simp_dungeon/templatetags/custom_filters.py
+++ news/simp_dungeon/templatetags/custom_filters.py
@@ -1,40 +1,30 @@
 from django import template
-from pymystem3 import Mystem
 from pygtrie import Trie
 import os
-# Импортируем стороннюю библиотеку которая будет цензурить русский язык
-# pymystem3 - мощный инструмент для работы с русским языком
-# Trie - будет уменьшать сложность
-# os - нужен для открытия файла с нежелательными словами
 
 register = template.Library()
-mystem = Mystem()
 
 @register.filter
 def censor(value):
     if isinstance(value, str):
-        # Загружаем список матерных слов в Trie
         censored_words = Trie()
 
         file_path = os.path.join(os.path.dirname(__file__), 'censored_words.txt')
         with open(file_path, 'r', encoding='utf-8') as file:
             for word in file:
-                # Убедимся, что добавляем только строки без пробелов
                 censored_words[word.strip()] = True
 
-        # Применяем lemmatize и убираем пробелы для каждого слова в результате
-        words = mystem.lemmatize(value)
-        words = [word.strip() for word in words if isinstance(word, str) and word.strip()]
-
+        words = value.split()
         censored_text = []
+
         for word in words:
-            # Проверяем, есть ли слово в списке цензурируемых, и заменяем его звездочками, если нужно
-            if word in censored_words:
-                censored_text.append('*' * len(word))
+            stripped_word = word.strip('.,!?')
+            if stripped_word in censored_words:
+                censored_word = word.replace(stripped_word, '*' * len(stripped_word))
+                censored_text.append(censored_word)
             else:
                 censored_text.append(word)
 
-        return ''.join(censored_text)
+        return ' '.join(censored_text)
 
     return value
-
