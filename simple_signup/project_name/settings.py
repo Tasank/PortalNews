6139986170f687470fd5e08a70a000658363b0d4
Tasank <apanasovich04@mail.ru>
2024-06-26 20:42:22 +0500

    {{secret_key}}
    Signed-off-by: Tasank <apanasovich04@mail.ru>

--- simple_signup/project_name/settings.py
+++ simple_signup/project_name/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*t7e9ctg#v(11c1=-90_03=u$nz0b#ela*!b)s54&3um)#=dt@'
+SECRET_KEY = '{{secret_key}}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
